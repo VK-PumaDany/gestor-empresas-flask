1739a7d9c86b0ed41a5e7609a740740f16414cf9
DanielPumarejoDev <j.pumarejo@nelumbo.com.co>
2026-05-12 18:55:51 -0500

    fix: adjust name

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "<h1>Bienvenido</h1>"
 
-@app.route("/saludo/nombre/<name>")
-def saludo(saludo):
-    return "<h1>Bienvenido {saludo}</h1>"
+
+@app.route("/saludo/nombre/<str:name>")
+def saludo(name):
+    # data = {"name": name}
+    return f"<h1>Bienvenido {name}</h1>"
+
 
 if __name__ == "__main__":
     app.run()
