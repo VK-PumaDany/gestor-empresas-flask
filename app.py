92f9bb0ba2f047a83e4efd22ba239f315c4e4b85
DanielPumarejoDev <j.pumarejo@nelumbo.com.co>
2026-05-14 18:12:10 -0500

    feat: add learn block and endBlock and includes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,11 +8,24 @@ def index():
     return "<h1>Bienvenido</h1>"
 
 
-@app.route("/saludo/nombre/<str:name>")
+@app.route("/page")
+def page():
+    cursos = ["HTML", "CSS", "Javascript", "Python", "MYSQL"]
+    data = {
+        "titulo_tab": "Mi primera Page con Flask",
+        "titulo_h1": "Bienvenid@!",
+        "cursos": cursos,
+        "all_cursos": len(cursos),
+    }
+    return render_template("index.html", data=data)
+
+
+@app.route("/saludo/nombre/<name>")
 def saludo(name):
+    print(name)
     # data = {"name": name}
-    return f"<h1>Bienvenido {name}</h1>"
+    return "<h1>Bienvenido</h1>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
