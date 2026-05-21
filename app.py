86f7098beb0671422279d1ed6d0748233eaa0d3b
DanVakano <vakanodan@gmail.com>
2026-05-21 13:24:33 -0500

    feat: add avances in the curso

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    print("Antes de las peticiones")
+
+
+@app.after_request
+def after_request(response):
+    print("Despues de la peticion")
+    return response
 
 
 @app.route("/")
@@ -26,14 +37,15 @@ def ladingFlask():
     }
     return render_template("landingFlask.html", data=data)
 
-@app.route("/calculator")
-def calculator():
-    data = {
-        "head": {
-            "title_tab": "Flask",
-        }
-    }
-    return render_template("calculator.html")
+
+# @app.route("/calculator")
+# def calculator():
+#     data = {
+#         "head": {
+#             "title_tab": "Flask",
+#         }
+#     }
+#     return render_template("calculator.html")
 
 
 @app.route("/welcome")
@@ -48,5 +60,31 @@ def saludo(name):
     return "<h1>Bienvenido</h1>"
 
 
+@app.route("/aprendizaje/<name>/<int:age>")
+def aprendizaje(name, age):
+    data = {
+        "head": {
+            "title_tab": "Apredizaje",
+        },
+        "name": name,
+        "age": age,
+    }
+    # query params :3
+    print(request.args)
+    print(request.args.get("sort"))
+    return render_template("./aprendizaje/aprendizaje.html", data=data)
+
+
+def not_found_page(error):
+    return render_template("404.html"), 404
+
+
+@app.route("/redireccionador")
+def redirect_page_index():
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
+    # app.add_url_rule('/route', view_func= name)
+    app.register_error_handler(404, not_found_page)
     app.run(debug=True, port=5000)
